0504a0d9ed82bbae356cf075c37bd6d334110bc5
Nikita Tepliakov <nikita_tepliakov@icloud.com>
2023-12-16 11:17:50 +0100

    downgrade swift version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DataStructuresPackage",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
